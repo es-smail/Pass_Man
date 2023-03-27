ba56069c56e6cdfc484f98f91fa2fc2c32d02dbb
med manour <manour12@gmail.com>
2023-03-27 15:04:39 +0000

    xml_project

---
+++ app/src/main/java/com/example/newproject/add_carte.java
@@ -0,0 +1,14 @@
+package com.example.newproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_carte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_carte);
+    }
+}
---
+++ app/src/main/java/com/example/newproject/update_carte.java
@@ -0,0 +1,14 @@
+package com.example.newproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class update_carte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.update_carte);
+    }
+}
